82889ee2972718599c45122d352e2282096d36a4
Catrin <catrinhughes92@gmail.com>
2019-11-21 15:10:41 +0000

    set up done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+  // const textTitle = document.querySelector('#title');
+  // textTitle.addEventListener('input', handleFormSubmit);
+  //
+  // const textAuthor = document.querySelector('#author');
+  // textAuthor.addEventListener('input', handleFormSubmit);
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+});
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  console.log(event);
+};
